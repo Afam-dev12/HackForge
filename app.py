4435b6a2d9e246488e9908f421f29eb1aac54735
Afam-dev12 <josephgloria017@gmail.com>
2026-06-23 15:40:46 +0100

    Setup Flask app and database models

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from config import Config
+from models import db
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")   
+def home():
+    return """
+    <h1>HackForge</h1>
+    <p>Forge Ideas. Build the Future.
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = "hackforge-secret-key"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hackforge.db"
+
+    SQLALCHEMY_TRACK_MODICATIONS = False
